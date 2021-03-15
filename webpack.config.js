3c7c04f3cbd65c30c774bdf95e96081bb280d21a
Roman Paska <roman@devima.solutions>
2021-03-15 15:35:33 +0200

    Build fixes for nodejs

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    libraryTarget: 'umd',
+    library: '@feedsdev/sdk',
+    libraryTarget: 'commonjs2',
     globalObject: 'this'
   },
   resolve: {
