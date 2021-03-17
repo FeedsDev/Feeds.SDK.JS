03531561078b750d87483141a88a24c2946f0ed4
Roman Paska <roman@devima.solutions>
2021-03-17 11:28:49 +0200

    API version update

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const USERS_API_URL = 'http://18.156.76.121:3003/api/v2'
-export const POSTS_API_URL = 'http://18.156.76.121:3002/api/v2'
-export const FEED_API_URL = 'http://18.156.76.121:3002/api/v1'
+export const USERS_API_URL = 'https://feeds-customers-2q5juf2a.ew.gateway.dev/api/v2'
+export const POSTS_API_URL = 'https://feeds-posts-2q5juf2a.ew.gateway.dev/api/v2'
+export const FEED_API_URL = 'https://feeds-posts-2q5juf2a.ew.gateway.dev/api/v1'
