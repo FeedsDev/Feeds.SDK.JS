f1898396b860a6b9c393c9838d18c36b165703aa
Roman Paska <roman@devima.solutions>
2021-03-25 15:37:32 +0200

    Version update

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const USERS_API_URL = 'https://workspace.api.feeds.dev/api/v2'
+export const USERS_API_URL = 'https://users.api.feeds.dev/api/v2'
 export const POSTS_API_URL = 'https://posts.api.feeds.dev/api/v2'
 export const FEED_API_URL = 'https://posts.api.feeds.dev/api/v1'
