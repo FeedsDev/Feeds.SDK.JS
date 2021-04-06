94642186e8bcc1c40fb0ba57c7836cbe6f5aef5c
Roman Paska <roman@devima.solutions>
2021-04-06 13:29:22 +0300

    Refresh token fixes

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,13 +4,22 @@ import { USERS_API_URL } from '../config'
 
 const axiosInstance = axios.create()
 
-let retry = false
-let refreshedData: IOptions
+const isRefreshToken = (url: string, workspaceId: number) => (
+  url === `${USERS_API_URL}/pages/${workspaceId}/customers/refreshToken`
+)
 
 export const setAxiosConfig = (config: IOptions): void => {
+
+  let refreshedData: IOptions
+  let retry = false
+
   axiosInstance.interceptors.request.use(
     (axiosConfig) => {
-      axiosConfig.headers.Authorization = `Bearer ${retry ? refreshedData.refreshToken : config.accessToken}`
+      if (isRefreshToken(axiosConfig.url!, config.workspaceId)) {
+        axiosConfig.headers.Authorization = `Bearer ${retry ? refreshedData.refreshToken : config.refreshToken}`
+      } else {
+        axiosConfig.headers.Authorization = `Bearer ${retry ? refreshedData.accessToken : config.accessToken}`
+      }
 
       return axiosConfig
     }
@@ -21,40 +30,39 @@ export const setAxiosConfig = (config: IOptions): void => {
     (error) => {
       const originalRequest = error.config
 
-      if (error.response.status === 401
-        && originalRequest.url === `${USERS_API_URL}/pages/${config.workspaceId}/customers/refreshToken`) {
+      if (error.response.status !== 401) {
         return Promise.reject(error)
       }
 
-      if (error.response.status === 401) {
-        axios.defaults.headers.common['Authorization'] = `Bearer ${retry ? refreshedData.refreshToken : config.refreshToken}`
-
-        retry = true
-
-        return axios.get(`${USERS_API_URL}/pages/${config.workspaceId}/customers/refreshToken`)
-          .then(res => {
-            const {
-              accessToken,
-              refreshToken
-            } = res.data
-
-            if (res.status === 200) {
-              refreshedData = {
-                ...refreshedData,
-                accessToken: accessToken,
-                refreshToken: refreshToken,
-                workspaceId: config.workspaceId
-              }
-
-              axiosInstance.defaults.headers.common['Authorization'] = 'Bearer ' + accessToken
-              originalRequest.headers['Authorization'] = 'Bearer ' + accessToken
-
-              return axiosInstance(originalRequest)
-            }
-            return axiosInstance(originalRequest)
-          })
+      if (isRefreshToken(originalRequest.url, config.workspaceId)) {
+        return Promise.reject(error)
       }
-      return Promise.reject(error)
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${retry ? refreshedData.refreshToken : config.refreshToken}`
+
+      retry = true
+
+      return axios.get(`${USERS_API_URL}/pages/${config.workspaceId}/customers/refreshToken`)
+        .then(res => {
+          const {
+            accessToken,
+            refreshToken
+          } = res.data
+
+          if (res.status === 200) {
+            refreshedData = {
+              ...refreshedData,
+              accessToken: accessToken,
+              refreshToken: refreshToken,
+              workspaceId: config.workspaceId
+            }
+
+            originalRequest.headers['Authorization'] = 'Bearer ' + accessToken
+
+            return axiosInstance(originalRequest)
+          }
+          return Promise.reject(error)
+        })
     }
   )
 }
